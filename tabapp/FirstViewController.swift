0b0284ee0fc92cf82c2c9d86caa6c4a4899c7991
kimi <kimi@kimideMacBook-Pro.local>
2017-06-03 18:03:33 +0800

    test

---
+++ tabapp/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  tabapp
+//
+//  Created by kimi on 2017/5/25.
+//  Copyright © 2017年 kimi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+     
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ tabapp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tabapp
+//
+//  Created by kimi on 2017/5/25.
+//  Copyright © 2017年 kimi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
